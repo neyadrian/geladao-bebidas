22e717cc2cddb143e3fd0258be1da3b51d8f039e
neyadrian <neyadrian2018@gmail.com>
2026-08-10 14:32:37 -0300

    Enum CategoriaProduto

---
+++ app/src/main/java/com/geladaobebidas/app/entities/Produto.java
@@ -0,0 +1,10 @@
+package com.geladaobebidas.app.entities;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Entity
+@Data
+public class Produto {
+
+}
---
+++ app/src/main/java/com/geladaobebidas/app/enums/CategoriaProduto.java
@@ -0,0 +1,5 @@
+package com.geladaobebidas.app.enums;
+
+public enum CategoriaProduto {
+    CERVEJA, REFRIGERANTE, AGUA, ENERGETICO, DESTILADO, VINHOS, WHISKY, VODKA, OUTROS;
+}
