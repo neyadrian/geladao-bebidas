0ba3f79f8f5ddcd6d1b533b72f153eb8422eeb91
neyadrian <neyadrian2018@gmail.com>
2026-08-10 14:53:45 -0300

    Criando entidade Produto

--- app/src/main/java/com/geladaobebidas/app/entities/Produto.java
+++ app/src/main/java/com/geladaobebidas/app/entities/Produto.java
@@ -1,10 +1,43 @@
 package com.geladaobebidas.app.entities;
 
-import jakarta.persistence.Entity;
+import com.geladaobebidas.app.enums.CategoriaProduto;
+import com.geladaobebidas.app.enums.TipoEmbalagem;
+import com.geladaobebidas.app.enums.UnidadeVolume;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.math.BigDecimal;
 
 @Entity
 @Data
 public class Produto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_produto")
+    private Long idProduto;
+
+    @Column(name = "nome_produto")
+    private String nomeProduto;
+
+    @Column(name = "preco_produto")
+    private BigDecimal precoProduto;
+
+    @Column(name = "quantidade_produto")
+    private Integer quantidadeProduto;
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "categoria_produto")
+    private CategoriaProduto categoriaProduto;
+
+    @Column(name = "volume_produto")
+    private Integer volumeProduto;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "unidade_volume_produto")
+    private UnidadeVolume unidadeVolumeProduto;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo_embalagem_produto")
+    private TipoEmbalagem tipoEmbalagemProduto;
 }
