fe2105a94b80c69d1dccaa67453250a8c7e44ab8
neyadrian <neyadrian2018@gmail.com>
2026-08-10 18:17:56 -0300

    Continuando entidade Venda

--- app/src/main/java/com/geladaobebidas/app/entities/Venda.java
+++ app/src/main/java/com/geladaobebidas/app/entities/Venda.java
@@ -1,10 +1,10 @@
 package com.geladaobebidas.app.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
@@ -12,5 +12,20 @@ public class Venda {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_venda")
     private Long idVenda;
+
+    @Column(name = "data_venda")
+    private LocalDateTime dataVenda;
+
+    @Column(name = "valor_total_venda")
+    private BigDecimal valorTotalVenda;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente")
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
 }
