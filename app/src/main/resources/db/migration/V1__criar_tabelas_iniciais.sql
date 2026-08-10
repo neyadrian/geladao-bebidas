5d8cd3c3493b790f63409354f9e2723469594862
neyadrian <neyadrian2018@gmail.com>
2026-08-10 12:24:57 -0300

    Modelando o Banco de Dados - Tabela ItemVenda

--- app/src/main/resources/db/migration/V1__criar_tabelas_iniciais.sql
+++ app/src/main/resources/db/migration/V1__criar_tabelas_iniciais.sql
@@ -31,8 +31,18 @@ CREATE TABLE venda (
     id_venda BIGINT AUTO_INCREMENT PRIMARY KEY,
     data_venda DATETIME NOT NULL,
     valor_total_venda DECIMAL(15, 2) NOT NULL,
-    id_cliente BIGINT,
+    id_cliente BIGINT NOT NULL,
     id_usuario BIGINT NOT NULL,
     FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
     FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+);
+
+CREATE TABLE item_venda (
+    id_item_venda BIGINT AUTO_INCREMENT PRIMARY KEY,
+    quantidade_item INT NOT NULL,
+    preco_unitario_item DECIMAL(15, 2) NOT NULL,
+    id_venda BIGINT NOT NUll,
+    id_produto BIGINT NOT NULL,
+    FOREIGN KEY (id_venda) REFERENCES venda(id_venda),
+    FOREIGN KEY (id_produto) REFERENCES produto(id_produto)
 );
