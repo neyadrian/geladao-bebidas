9c39c27d775dfe712822e2684422916d787eefaa
neyadrian <neyadrian2018@gmail.com>
2026-08-11 14:27:13 -0300

    Criando e Finalizando ClienteService

---
+++ app/src/main/java/com/geladaobebidas/app/services/ClienteService.java
@@ -0,0 +1,33 @@
+package com.geladaobebidas.app.services;
+
+import com.geladaobebidas.app.entities.Cliente;
+import com.geladaobebidas.app.repositories.ClienteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    public void salvar(Cliente cliente) {
+        clienteRepository.save(cliente);
+    }
+
+    public List<Cliente> buscarPorNome(String nome) {
+        return clienteRepository.findByNomeCliente(nome);
+    }
+
+    public List<Cliente> buscarTodos() {
+        return clienteRepository.findAll();
+    }
+
+    public void excluir(Long id) {
+        clienteRepository.deleteById(id);
+    }
+}
