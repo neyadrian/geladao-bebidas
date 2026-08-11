d9137fe57c867bdb4929beb4aad8f27a780eb3a9
neyadrian <neyadrian2018@gmail.com>
2026-08-11 13:21:19 -0300

    Criando Repoitories - VendaRepository

--- app/src/main/java/com/geladaobebidas/app/repositories/VendaRepository.java
+++ app/src/main/java/com/geladaobebidas/app/repositories/VendaRepository.java
@@ -1,9 +1,9 @@
 package com.geladaobebidas.app.repositories;
 
-import com.geladaobebidas.app.entities.Usuario;
+import com.geladaobebidas.app.entities.Venda;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface VendaRepository extends JpaRepository<Venda, Long> {
 }
