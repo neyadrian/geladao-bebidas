c7ece9634801c47a1cc9547efc9a91605c368663
neyadrian <neyadrian2018@gmail.com>
2026-08-11 14:51:12 -0300

    Criando e Finalizando UsuarioService

---
+++ app/src/main/java/com/geladaobebidas/app/services/UsuarioService.java
@@ -0,0 +1,33 @@
+package com.geladaobebidas.app.services;
+
+import com.geladaobebidas.app.entities.Usuario;
+import com.geladaobebidas.app.repositories.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public void salvar(Usuario usuario) {
+        usuarioRepository.save(usuario);
+    }
+
+    public Usuario buscarPorId(Long id) {
+        return usuarioRepository.findById(id).orElse(null);
+    }
+
+    public List<Usuario> listarTodos() {
+        return usuarioRepository.findAll();
+    }
+
+    public void excluir(Usuario usuario) {
+        usuarioRepository.delete(usuario);
+    }
+}
