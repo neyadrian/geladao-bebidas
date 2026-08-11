b64b23d7b1ba6e653f0a5bce9a01aac6257d343d
neyadrian <neyadrian2018@gmail.com>
2026-08-11 08:28:10 -0300

    Entidade ItemVenda

---
+++ app/src/main/java/com/geladaobebidas/app/entities/ItemVenda.java
@@ -0,0 +1,30 @@
+package com.geladaobebidas.app.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+public class ItemVenda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_item_venda")
+    private Long idItemVenda;
+
+    @Column(name = "quantidade_item")
+    private int quantidadeItem;
+
+    @Column(name = "preco_unitario_item")
+    private BigDecimal precoUnitarioItem;
+
+    @ManyToOne
+    @JoinColumn(name = "id_venda", nullable = false)
+    private Venda venda;
+
+    @ManyToOne
+    @JoinColumn(name = "id_produto", nullable = false)
+    private Produto produto;
+}
