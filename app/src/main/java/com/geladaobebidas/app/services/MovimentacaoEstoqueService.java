59d960697fce49e9f3547f1c65f9dd101829bea4
neyadrian <neyadrian2018@gmail.com>
2026-08-12 08:40:41 -0300

    Criando e Finalizando MovimentacaoEstoqueService

--- app/src/main/java/com/geladaobebidas/app/services/MovimentacaoEstoqueService.java
+++ app/src/main/java/com/geladaobebidas/app/services/MovimentacaoEstoqueService.java
@@ -1,4 +1,35 @@
 package com.geladaobebidas.app.services;
 
+import com.geladaobebidas.app.entities.MovimentacaoEstoque;
+import com.geladaobebidas.app.exceptions.RecursoNaoEncontradoException;
+import com.geladaobebidas.app.repositories.MovimentacaoEstoqueRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MovimentacaoEstoqueService {
+
+    private final MovimentacaoEstoqueRepository movimentacaoEstoqueRepository;
+
+    public MovimentacaoEstoqueService(MovimentacaoEstoqueRepository movimentacaoEstoqueRepository) {
+        this.movimentacaoEstoqueRepository = movimentacaoEstoqueRepository;
+    }
+
+    public void salvar(MovimentacaoEstoque movimentacaoEstoque) {
+        movimentacaoEstoqueRepository.save(movimentacaoEstoque);
+    }
+
+    public MovimentacaoEstoque buscarPorId(Long id) {
+        return movimentacaoEstoqueRepository.findById(id)
+                .orElseThrow(() -> new RecursoNaoEncontradoException("Movimentação não encontrado: " + id));
+    }
+
+    public List<MovimentacaoEstoque> listarTodas() {
+        return movimentacaoEstoqueRepository.findAll();
+    }
+
+    public void excluir(Long id) {
+        movimentacaoEstoqueRepository.deleteById(id);
+    }
 }
