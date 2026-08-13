3f2f27b65db3ea0d716fdfb211e1328d6f532589
neyadrian <neyadrian2018@gmail.com>
2026-08-13 14:25:20 -0300

    API REST - criando UsuarioController

---
+++ app/src/main/java/com/geladaobebidas/app/controllers/UsuarioController.java
@@ -0,0 +1,51 @@
+package com.geladaobebidas.app.controllers;
+
+import com.geladaobebidas.app.entities.Cliente;
+import com.geladaobebidas.app.entities.Usuario;
+import com.geladaobebidas.app.services.UsuarioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
+        usuarioService.salvar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
+    }
+
+    @PutMapping
+    public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario) {
+        usuarioService.salvar(usuario);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscarPorId(@PathVariable Long id) {
+        Usuario usuario = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> buscarTodos() {
+        List<Usuario> usuarios = usuarioService.listarTodos();
+        return ResponseEntity.ok(usuarios);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable Long id) {
+        usuarioService.excluir(id);
+        return ResponseEntity.noContent().build();
+    }
+}
