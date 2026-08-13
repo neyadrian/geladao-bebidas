7a3ca1ad4b458b00170ec38f3dc187e59df6872b
neyadrian <neyadrian2018@gmail.com>
2026-08-13 13:49:04 -0300

    API REST - criando ProdutoController

---
+++ app/src/main/java/com/geladaobebidas/app/controllers/ProdutoController.java
@@ -0,0 +1,44 @@
+package com.geladaobebidas.app.controllers;
+
+import com.geladaobebidas.app.entities.Produto;
+import com.geladaobebidas.app.services.ProdutoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> criar(@RequestBody Produto produto) {
+        produtoService.salvar(produto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(produto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> buscarPorId(@PathVariable Long id) {
+        Produto produto = produtoService.buscarPorId(id);
+        return ResponseEntity.ok(produto);
+    }
+
+    @GetMapping
+    private ResponseEntity<List<Produto>> listarTodos() {
+        return ResponseEntity.ok(produtoService.listarTodos());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable Long id) {
+        produtoService.excluir(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
