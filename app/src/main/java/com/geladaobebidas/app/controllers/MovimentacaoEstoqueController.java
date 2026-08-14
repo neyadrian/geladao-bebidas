dfe87bf6f8b2c5b89a0c630248add3829e9cb43e
neyadrian <neyadrian2018@gmail.com>
2026-08-14 08:09:15 -0300

    API REST - criando MovimentacaoEstoqueController

---
+++ app/src/main/java/com/geladaobebidas/app/controllers/MovimentacaoEstoqueController.java
@@ -0,0 +1,43 @@
+package com.geladaobebidas.app.controllers;
+
+import com.geladaobebidas.app.entities.MovimentacaoEstoque;
+import com.geladaobebidas.app.services.MovimentacaoEstoqueService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movimentacao-estoque")
+public class MovimentacaoEstoqueController {
+
+    private final MovimentacaoEstoqueService  movimentacaoEstoqueService;
+
+    public MovimentacaoEstoqueController(MovimentacaoEstoqueService movimentacaoEstoqueService) {
+        this.movimentacaoEstoqueService = movimentacaoEstoqueService;
+    }
+
+    @PostMapping
+    public ResponseEntity<MovimentacaoEstoque> salvar(@RequestBody MovimentacaoEstoque movimentacaoEstoque) {
+        movimentacaoEstoqueService.salvar(movimentacaoEstoque);
+        return  ResponseEntity.status(HttpStatus.CREATED).body(movimentacaoEstoque);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MovimentacaoEstoque> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok().body(movimentacaoEstoqueService.buscarPorId(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MovimentacaoEstoque>> listarTodas() {
+        List<MovimentacaoEstoque> movimentacaoEstoque = movimentacaoEstoqueService.listarTodas();
+        return ResponseEntity.ok().body(movimentacaoEstoque);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable Long id) {
+        movimentacaoEstoqueService.excluir(id);
+        return ResponseEntity.noContent().build();
+    }
+}
