ba978edac814d6036c19e7725a2547ec2ceed998
neyadrian <neyadrian2018@gmail.com>
2026-08-15 20:30:39 -0300

    Update no VendaRepository

--- app/src/main/java/com/geladaobebidas/app/repositories/VendaRepository.java
+++ app/src/main/java/com/geladaobebidas/app/repositories/VendaRepository.java
@@ -4,6 +4,11 @@ import com.geladaobebidas.app.entities.Venda;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface VendaRepository extends JpaRepository<Venda, Long> {
+    List<Venda> findByDataVendaBetween(LocalDateTime inicio, LocalDateTime fim);
+
 }
