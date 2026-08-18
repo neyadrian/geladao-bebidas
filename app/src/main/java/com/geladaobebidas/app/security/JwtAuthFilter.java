beda52902d364b139ae0df80c246a1ecbb8d51ee
neyadrian <neyadrian2018@gmail.com>
2026-08-18 14:12:52 -0300

    Criando JwtAuthFilter

---
+++ app/src/main/java/com/geladaobebidas/app/security/JwtAuthFilter.java
@@ -0,0 +1,24 @@
+package com.geladaobebidas.app.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    public JwtAuthFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        filterChain.doFilter(request, response);
+    }
+}
