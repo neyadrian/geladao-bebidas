139233a75d004a92021454f3de6fc478c3d73eae
neyadrian <neyadrian2018@gmail.com>
2026-08-20 13:57:52 -0300

    Atualizando UsuarioServiceTest

--- app/src/test/java/com/geladaobebidas/app/services/UsuarioServiceTest.java
+++ app/src/test/java/com/geladaobebidas/app/services/UsuarioServiceTest.java
@@ -1,6 +1,7 @@
 package com.geladaobebidas.app.services;
 
 import com.geladaobebidas.app.entities.Usuario;
+import com.geladaobebidas.app.exceptions.RecursoNaoEncontradoException;
 import com.geladaobebidas.app.repositories.UsuarioRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -40,4 +42,15 @@ class UsuarioServiceTest {
         assertEquals("Usuario Teste", resultado.getNomeUsuario());
 
     }
+
+    @Test
+    void buscarPorIdQuandoNaoEncontrado() {
+        when(usuarioRepository.findById(1L)).thenReturn(Optional.empty());
+
+        assertThrows(RecursoNaoEncontradoException.class, () -> {
+            usuarioService.buscarPorId(1L);
+        });
+    }
+
+    
 }
