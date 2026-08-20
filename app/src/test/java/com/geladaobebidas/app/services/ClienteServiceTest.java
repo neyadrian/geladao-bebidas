ee2eb33f038be93e866ac6918f76af127927f4c7
neyadrian <neyadrian2018@gmail.com>
2026-08-20 13:18:13 -0300

    Criando ClienteServiceTest

---
+++ app/src/test/java/com/geladaobebidas/app/services/ClienteServiceTest.java
@@ -0,0 +1,19 @@
+package com.geladaobebidas.app.services;
+
+import com.geladaobebidas.app.repositories.ClienteRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ClienteServiceTest {
+
+    @Mock
+    private ClienteRepository clienteRepository;
+
+    @InjectMocks
+    private ClienteService clienteService;
+
+}
