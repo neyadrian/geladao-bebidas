ca0062b5cb09efbe193d707156828acf87403a2e
neyadrian <neyadrian2018@gmail.com>
2026-08-21 14:35:49 -0300

    Atualizando MovimentacaoEstoqueServiceTest

--- app/src/test/java/com/geladaobebidas/app/services/MovimetacaoEstoqueServiceTest.java
+++ app/src/test/java/com/geladaobebidas/app/services/MovimetacaoEstoqueServiceTest.java
@@ -1,9 +1,17 @@
 package com.geladaobebidas.app.services;
 
+import com.geladaobebidas.app.entities.MovimentacaoEstoque;
+import com.geladaobebidas.app.enums.TipoMovimentacao;
 import com.geladaobebidas.app.repositories.MovimentacaoEstoqueRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class MovimetacaoEstoqueServiceTest {
@@ -13,4 +21,17 @@ public class MovimetacaoEstoqueServiceTest {
 
     @Mock
     private MovimentacaoEstoqueService movimentacaoEstoqueService;
+
+    @Test
+    void deveRetornarMovimentacaoQuandoEncontrada() {
+        MovimentacaoEstoque movimentacaoFake = criarMovimentacaoFake(1L, TipoMovimentacao.ENTRADA, 10);
+
+        when(movimentacaoEstoqueRepository.findById(1L)).thenReturn(Optional.of(movimentacaoFake));
+
+        MovimentacaoEstoque resultado = movimentacaoEstoqueService.buscarPorId(1L);
+
+        assertEquals(1L, resultado.getIdMovimentacao());
+        assertEquals(TipoMovimentacao.ENTRADA, resultado.getTipoMovimentacao());
+        assertEquals(10, resultado.getQuantidadeMovimentacao());
+    }
 }
