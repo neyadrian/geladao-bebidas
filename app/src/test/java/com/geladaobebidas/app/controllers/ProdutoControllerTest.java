7e4f20054927a99c93baf1e94bdd619f9bb66c34
neyadrian <neyadrian2018@gmail.com>
2026-08-22 12:39:46 -0300

    Atualizando ProdutoControllerTest

--- app/src/test/java/com/geladaobebidas/app/controllers/ProdutoControllerTest.java
+++ app/src/test/java/com/geladaobebidas/app/controllers/ProdutoControllerTest.java
@@ -1,12 +1,19 @@
 package com.geladaobebidas.app.controllers;
 
+import com.geladaobebidas.app.entities.Produto;
 import com.geladaobebidas.app.services.ProdutoService;
+import org.junit.jupiter.api.MediaType;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import tools.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ProdutoController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -20,4 +27,17 @@ public class ProdutoControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Test
+    void deveCriarProdutoComSucesso() throws Exception {
+        Produto produto = new Produto();
+        produto.setIdProduto(1L);
+        produto.setNomeProduto("Cerveja Teste");
+
+        mockMvc.perform(post("/produtos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(produto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.nomeProduto").value("Cerveja Teste"));
+    }
 }
