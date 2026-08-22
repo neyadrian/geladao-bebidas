0ffa252281dc7f55b6a2692d5291731c99ba5906
neyadrian <neyadrian2018@gmail.com>
2026-08-22 12:22:52 -0300

    Corrigindo Anotation em MovimentacaoEstoqueServiceTest

--- app/src/test/java/com/geladaobebidas/app/services/MovimentacaoEstoqueServiceTest.java
+++ app/src/test/java/com/geladaobebidas/app/services/MovimentacaoEstoqueServiceTest.java
@@ -21,7 +21,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class MovimetacaoEstoqueServiceTest {
+class MovimentacaoEstoqueServiceTest {
 
     @Mock
     private MovimentacaoEstoqueRepository movimentacaoEstoqueRepository;
