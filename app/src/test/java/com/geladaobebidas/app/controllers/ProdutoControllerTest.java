2110a17ef24412433ca9d474481d44588d26afa0
neyadrian <neyadrian2018@gmail.com>
2026-08-22 12:42:52 -0300

    Atualizando ProdutoControllerTest

--- app/src/test/java/com/geladaobebidas/app/controllers/ProdutoControllerTest.java
+++ app/src/test/java/com/geladaobebidas/app/controllers/ProdutoControllerTest.java
@@ -1,5 +1,6 @@
 package com.geladaobebidas.app.controllers;
 
+import ch.qos.logback.core.encoder.EchoEncoder;
 import com.geladaobebidas.app.entities.Produto;
 import com.geladaobebidas.app.services.ProdutoService;
 import org.junit.jupiter.api.MediaType;
@@ -11,6 +12,8 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import tools.jackson.databind.ObjectMapper;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -38,6 +41,19 @@ public class ProdutoControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(produto)))
                 .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.nomeProduto").value("Cerveja Teste"));
+    }
+
+    @Test
+    void deveBuscarProdutoPorIdComSucesso() throws Exception {
+        Produto produto = new Produto();
+        produto.setIdProduto(1L);
+        produto.setNomeProduto("Cerveja Teste");
+
+        when(produtoService.buscarPorId(1L)).thenReturn(produto);
+
+        mockMvc.perform(get("/produtos/1"))
+                .andExpect(status().isOk())
                 .andExpect(jsonPath("$.nomeProduto").value("Cerveja Teste"));
     }
 }
