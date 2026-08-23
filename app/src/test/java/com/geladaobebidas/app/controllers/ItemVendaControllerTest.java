31a5369ff5c6c1960fb7d66e5ec1c0f3f6713965
neyadrian <neyadrian2018@gmail.com>
2026-08-23 18:20:40 -0300

    Atualizando ItemVendaControllerTest

--- app/src/test/java/com/geladaobebidas/app/controllers/ItemVendaControllerTest.java
+++ app/src/test/java/com/geladaobebidas/app/controllers/ItemVendaControllerTest.java
@@ -1,4 +1,53 @@
 package com.geladaobebidas.app.controllers;
 
+import com.geladaobebidas.app.entities.ItemVenda;
+import com.geladaobebidas.app.security.JwtService;
+import com.geladaobebidas.app.services.ItemVendaService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.http.RequestEntity.post;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ItemVendaController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class ItemVendaControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private ItemVendaService itemVendaService;
+
+    @MockitoBean
+    private JwtService jwtService;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void deveCriarItemVendaComSucesso() throws Exception {
+        ItemVenda itemVenda = criarItemVenda(1L, 2, "10.50");
+
+        doNothing().when(itemVendaService).salvar(any(ItemVenda.class));
+
+        mockMvc.perform(post("/itens-venda")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(itemVenda)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.idItemVenda").value(1L))
+                .andExpect(jsonPath("$.quantidadeItem").value(2))
+                .andExpect(jsonPath("$.precoUnitarioItem").value(10.50));
+
+    }
+
+    
 }
