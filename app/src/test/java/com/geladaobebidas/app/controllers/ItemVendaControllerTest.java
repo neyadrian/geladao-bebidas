0cbf0c12dee218b3bfb8b2d7f2765c1f43140cc1
neyadrian <neyadrian2018@gmail.com>
2026-08-23 18:22:50 -0300

    Atualizando ItemVendaControllerTest

--- app/src/test/java/com/geladaobebidas/app/controllers/ItemVendaControllerTest.java
+++ app/src/test/java/com/geladaobebidas/app/controllers/ItemVendaControllerTest.java
@@ -14,8 +14,10 @@ import tools.jackson.databind.ObjectMapper;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import static org.springframework.http.RequestEntity.post;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ItemVendaController.class)
@@ -49,5 +51,16 @@ public class ItemVendaControllerTest {
 
     }
 
-    
+    @Test
+    void deveBuscarItemVendaPorIdComSucesso() throws Exception {
+        ItemVenda itemVenda = criarItemVenda(1L, 2, "10.50");
+
+        when(itemVendaService.buscarPorId(1L)).thenReturn(itemVenda);
+
+        mockMvc.perform(get("/itens-venda/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.idItemVenda").value(1L))
+                .andExpect(jsonPath("$.quantidadeItem").value(2))
+                .andExpect(jsonPath("$.precoUnitarioItem").value(10.50));
+    }
 }
