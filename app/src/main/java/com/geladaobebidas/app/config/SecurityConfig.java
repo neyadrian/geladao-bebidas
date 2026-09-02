2aac422214f3468d8be64717bc2c96d96b31afff
neyadrian <neyadrian2018@gmail.com>
2026-09-02 08:29:22 -0300

    fix: Liberando CORS para o vercel

--- app/src/main/java/com/geladaobebidas/app/config/SecurityConfig.java
+++ app/src/main/java/com/geladaobebidas/app/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
